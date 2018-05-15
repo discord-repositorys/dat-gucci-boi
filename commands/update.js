0b6458d98948657719a63c3220d394ab8127aedc
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-15 19:02:46 -0400

    add update

--- commands/update.js
+++ commands/update.js
@@ -1,1 +1,24 @@
+const exec = require('child_process').exec;
+
+exports.run = async (client, message, args, level) => {
+    exec("git pull", (error, stdout) => {
+        const response = (error || stdout);
+        message.channel.send("Bot is updated, and READY TO ROLL! :white_check_mark:").catch(console.error);
+    });
+};
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "Bot Admin"
+};
+
+exports.help = {
+    name: "update",
+    category: "System",
+    description: "Updates the bot",
+    usage: "update"
+};
 
