82a6b93d081aaa6537aae91d68e0e1443623ed00
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-15 19:20:49 -0400

    unl0ad rel0ad in update

--- commands/update.js
+++ commands/update.js
@@ -3,8 +3,14 @@ const exec = require('child_process').exec;
 exports.run = async (client, message, args, level) => {
     exec("git pull", (error, stdout) => {
         const response = (error || stdout);
-        message.channel.send("Bot is updated, and READY TO ROLL! :white_check_mark:").catch(console.error);
     });
+    let resp = await client.unloadCommand(args[0]);
+    if (resp) return message.reply(`Error Unloading: ${resp}`);
+
+    let r = client.loadCommand(args[0]);
+    if (r) return message.reply(`Error Loading: ${r}`);
+    
+    message.channel.send("Bot is updated, and READY TO ROLL! :white_check_mark:").catch(console.error);
 };
 
 
