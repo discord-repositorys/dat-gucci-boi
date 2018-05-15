3f20b653c717a85f808e6241393f61e8b8835d17
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-15 17:27:41 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+// This will check if the node version you are running is the required
+// Node version, if it isn't it will throw the following error to inform
+// you.
+if (process.version.slice(1).split(".")[0] < 8) throw new Error("Node 8.0.0 or higher is required. Update Node on your system.");
+
+// Load up the discord.js library
+const Discord = require("discord.js");
+// We also load the rest of the things we need in this file:
+const { promisify } = require("util");
+const readdir = promisify(require("fs").readdir);
+const Enmap = require("enmap");
+const EnmapLevel = require("enmap-level");
+
+
+const client = new Discord.Client();
+
+
+client.config = require("./config.js");
+
+
+
+client.logger = require("./util/Logger");
+
+
+require("./modules/functions.js")(client);
+
+
+client.commands = new Enmap();
+client.aliases = new Enmap();
+
+client.settings = new Enmap({provider: new EnmapLevel({name: "settings"})});
+
+
+
+const init = async () => {
+
+  const cmdFiles = await readdir("./commands/");
+  client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
+  cmdFiles.forEach(f => {
+    if (!f.endsWith(".js")) return;
+    const response = client.loadCommand(f);
+    if (response) console.log(response);
+  });
+
+
+  const evtFiles = await readdir("./events/");
+  client.logger.log(`Loading a total of ${evtFiles.length} events.`);
+  evtFiles.forEach(file => {
+    const eventName = file.split(".")[0];
+    const event = require(`./events/${file}`);
+
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
+  client.on("error", (err) => client.logger.error(err)); 
+  
+  // client.on("ready", () => {
+  //   console.log("Bot is online, and READY TO ROLL!");
+  //   client.setActivity("why JS? Just WHY?!");
+  //   client.setStatus("idle");
+  // });
+
+  client.levelCache = {};
+  for (let i = 0; i < client.config.permLevels.length; i++) {
+    const thisLevel = client.config.permLevels[i];
+    client.levelCache[thisLevel.name] = thisLevel.level;
+  }
+
+  client.login(client.config.token);
+
+// End top-level async/await function.
+};
+
+init();
