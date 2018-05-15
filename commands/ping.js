2ed4bfe4fb47aaea3e6e4fdf9ed456caff9c5792
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-15 19:44:12 -0400

    possible fix for ping

--- commands/ping.js
+++ commands/ping.js
@@ -3,14 +3,14 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   const embed = new Discord.RichEmbed()
     .setTitle("Pinging...")
     .setDescription("Hang in there!")
-    .setColor(0x00ff00)
+    .setColor(0x00ff00);
   const msg = message.channel.send({ embed }).then(m => {
   const em = new Discord.RichEmbed()
     .setTitle("PoIIIING!")
     .setDescription(`Bot Latency: ${msg.createdTimestamp - message.createdTimestamp}ms\nAPI Latency: ${Math.round(client.ping)}ms`)
     .setColor(0x00ff00)
     .setAuthor(`Sent by ${message.author.username}`, message.author.displayAvatarURL);
-  m.edit(embed);
+  m.edit({em});
   });
   
 };
