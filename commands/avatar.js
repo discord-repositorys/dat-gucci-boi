e795dc2a4b7247c7890f3c43408c5c4354a2bfbd
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-17 18:37:45 -0400

    add avatar cmd

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,1 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+    let user = user || args[0];
+    const embed = new MessageEmbed()
+        .setTitle(`${user.name}'s avatar`)
+        .setImage(user.defaultAvatarURL);
+    message.channel.send(embed);
+};
+
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["av"],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "avatar",
+    category: "Utility",
+    description: "Gets a user's avatar picture!",
+    usage: "avatar <user>"
+};
 
