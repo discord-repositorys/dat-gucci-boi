dc34ae9fef388455b79b6f47c5aa25d831a7476f
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-21 14:58:02 -0400

    shet my bad :(

--- commands/blame.js
+++ commands/blame.js
@@ -1,7 +1,7 @@
 const { MessageAttachment } = require("discord.js");
 exports.run = async (client, message, args, level) => {
     await message.channel.send(new MessageAttachment(
-        await client.idioticapi.batSlap(message.author.displayAvatarURL({ format: "png", size: 128 }),
+        await client.idiotic.batSlap(message.author.displayAvatarURL({ format: "png", size: 128 }),
             message.mentions.users.first().displayAvatarURL({ format: "png", size: 128 })),
         "blame.png"));
 };
