3c17f86ca3f40d7727084378f47bee389df94748
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-21 17:09:06 -0400

    create userinfo

---
+++ commands/userinfo.js
@@ -0,0 +1,35 @@
+exports.run = async (client, message, args, level) => {
+    const { MessageEmbed } = require("discord.js");
+    const moment = require("moment");
+    require("moment-duration-format");
+    const user = message.mentions.users.first() || await client.fetchUser(args.split(" ")[0]) || message.author;
+    const created_time = moment.duration(Date.now() - message.author.createdTimestamp).format("Y [years], M [months], D [days], H [hrs], m [mins], s [secs]");
+    const embed = new MessageEmbed()
+        .setTitle("User Info")
+        .setColor(0x00ff00)
+        .setAuthor(`${user.tag}`, user.displayAvatarURL({ format: "png", size: 128 }))
+        .addField("ID", user.id)
+        .addField("Registration Date", created_time)
+        .addField("Currently Playing", user.presence.game)
+        .addField("Status", user.presence.status)
+        user.bot ? embed.addField("Bot", "Yes") : embed.addField("Bot", "No");
+        user.verified ? embed.addField("Verified User", "Yes") : embed.addField("Verified User", "No");
+        
+        
+        
+    message.channel.send(embed);
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["8ball"],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "eightball",
+    category: "Fun",
+    description: "Not sure? Be sure! Ask the 8ball!",
+    usage: ".eightball question"
+};
