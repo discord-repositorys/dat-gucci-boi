ee15243a538408992749ae255dc57359b63a0085
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-21 18:54:58 -0400

    added some idiotic endpoints

---
+++ commands/snapchat.js
@@ -0,0 +1,16 @@
+const { MessageAttachment } = require("discord.js");
+exports.run = async (client, message, args, level) => {
+    await message.channel.send(new MessageAttachment(
+        await client.idiotic.snapchat(args.join(" ")), "snapchat.png"));
+};
+exports.conf = {
+    aliases: [],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "snapchat",
+    category: "Idiotic",
+    description: "A Snap with the best picture ever.",
+    usage: "snapchat [text]"
+};
--- commands/steam.js
+++ commands/steam.js
@@ -1,8 +1,8 @@
 const { MessageAttachment } = require("discord.js");
 exports.run = async (client, message, args, level) => {
     const user = message.mentions.users.first() || await client.fetchUser(args[0]) || message.author;
-    await message.channel.send(`**${user.username}** is beautiful!`, new MessageAttachment(
-        await client.idiotic.beautiful(message.author.displayAvatarURL({ format: "png", size: 128 })), "beautiful.png"));
+    await message.channel.send(new MessageAttachment(
+        await client.idiotic.steam(user.displayAvatarURL({ format: "png", size: 128 }), user.username), "achievement.png"));
 };
 exports.conf = {
     aliases: [],
@@ -10,8 +10,8 @@ exports.conf = {
 };
 
 exports.help = {
-    name: "beautiful",
+    name: "steam",
     category: "Idiotic",
-    description: "Become BEAUTIFUL!",
-    usage: "beautiful"
+    description: "Right on a Steam card!",
+    usage: "steam <user>"
 };
