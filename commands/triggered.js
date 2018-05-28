0e22336116fc93ea918c1e44ef0305a98f92b26a
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-28 16:09:02 -0400

    change to gif

--- commands/triggered.js
+++ commands/triggered.js
@@ -2,8 +2,8 @@ const { MessageAttachment } = require("discord.js");
 exports.run = async (client, message, args, level) => {
     message.channel.startTyping(1);
     const user = message.mentions.users.first() || message.author;
-    await message.channel.send(`**${user.username}**is triggered. Very triggered.`, new MessageAttachment(
-        await client.idiotic.triggered(user.displayAvatarURL({ format: "png", size: 128 })), "triggered.png"));
+    await message.channel.send(`**${user.username}** is triggered. Very triggered.`, new MessageAttachment(
+        await client.idiotic.triggered(user.displayAvatarURL({ format: "png", size: 128 })), "triggered.gif"));
     message.channel.stopTyping();
 },
     exports.conf = {
