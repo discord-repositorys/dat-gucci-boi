ff02e13d78dfc04db10dddd4f3f75515dcfc67bf
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-28 16:29:57 -0400

    add crush

---
+++ commands/crush.js
@@ -0,0 +1,23 @@
+const { MessageAttachment } = require("discord.js");
+exports.run = async (client, message, args, level) => {
+    message.channel.startTyping(1);
+    const user = message.mentions.users.first();
+    if (!user) {
+        message.channel.send("Please tag a user to SLAP THEM!");
+        return;
+    }
+    await message.channel.send(`owo! **${message.author.username}** has a crush on **${user.username}**! :revolving_hearts:`, new MessageAttachment(
+        await client.idiotic.crush(message.author.displayAvatarURL({ format: "png", size: 128 }), user.displayAvatarURL({ format: "png", size: 128 })), "crush.png"));
+    message.channel.stopTyping();
+},
+    exports.conf = {
+        aliases: [],
+        permLevel: "User"
+    };
+
+exports.help = {
+    name: "crush",
+    category: "Idiotic",
+    description: "owo! Who's your crush...",
+    usage: "crush [user]"
+};
