4efdf3effa2300b28436be90623f629c555df968
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-28 16:07:41 -0400

    this fix will stop the bot from crashing like hell

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -12,5 +12,7 @@ module.exports = (client, member) => {
 
   // Send the welcome message to the welcome channel
   // There's a place for more configs here.
-  member.guild.channels.find("name", settings.welcomeChannel).send(welcomeMessage).catch(console.error);
+  if (settings.welcomeChannel){
+    member.guild.channels.find("name", settings.welcomeChannel).send(welcomeMessage).catch(console.error);
+  }
 };
