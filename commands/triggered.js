140bf1bd5c46b4757828f7c989f66a290822bde1
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-28 16:04:57 -0400

    the cmd that y'all been waiting for...triggered

---
+++ commands/triggered.js
@@ -0,0 +1,19 @@
+const { MessageAttachment } = require("discord.js");
+exports.run = async (client, message, args, level) => {
+    message.channel.startTyping(1);
+    const user = message.mentions.users.first() || message.author;
+    await message.channel.send(`**${user.username}**is triggered. Very triggered.`, new MessageAttachment(
+        await client.idiotic.triggered(user.displayAvatarURL({ format: "png", size: 128 })), "triggered.png"));
+    message.channel.stopTyping();
+},
+    exports.conf = {
+        aliases: [],
+        permLevel: "User"
+    };
+
+exports.help = {
+    name: "triggered",
+    category: "Idiotic",
+    description: "GRRR...GET TRIGGERED!",
+    usage: "triggered <user>"
+};
