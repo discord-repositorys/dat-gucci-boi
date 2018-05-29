6e5ff3f14ec8f94a8ea7442f715bc055ba20fbef
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-29 17:44:22 -0400

    wrong text smh

--- commands/rejected.js
+++ commands/rejected.js
@@ -2,7 +2,7 @@ const { MessageAttachment } = require("discord.js");
 exports.run = async (client, message, args, level) => {
     message.channel.startTyping(1);
     const user = message.mentions.users.first() || message.author;
-    await message.channel.send(`**${user.username}** is now approved.`, new MessageAttachment(
+    await message.channel.send(`OOF...**${user.username}** is now rejected.`, new MessageAttachment(
         await client.idiotic.rejected(user.displayAvatarURL({ format: "png", size: 128 })), "rejected.png"));
     message.channel.stopTyping();
 },
