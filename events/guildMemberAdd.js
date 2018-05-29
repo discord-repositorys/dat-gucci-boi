20def15917f2a42679b7de0116481e11d4c02b3a
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-28 20:06:00 -0400

    fix bot from crashing

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,18 +1,12 @@
-// This event executes when a new member joins a server. Let's welcome them!
-
 module.exports = (client, member) => {
-  // Load the guild's settings
   const settings = client.settings.get(member.guild.id);
 
-  // If welcome is off, don't proceed (don't welcome the user)
   if (settings.welcomeEnabled !== "true") return;
 
-  // Replace the placeholders in the welcome message with actual data
   const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.tag);
 
-  // Send the welcome message to the welcome channel
-  // There's a place for more configs here.
-  if (settings.welcomeChannel){
-    member.guild.channels.find("name", settings.welcomeChannel).send(welcomeMessage).catch(console.error);
+  if (settings.welcomeChannel) {
+    const chan = member.guild.channels.find("name", settings.welcomeChannel);
+    if (chan) chan.send(welcomeMessage).catch(console.error); // README
   }
 };
