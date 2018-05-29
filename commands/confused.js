8480b60193a876b4ec879f075c9d98963daddbcb
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-05-28 21:04:14 -0400

    wtf

--- commands/confused.js
+++ commands/confused.js
@@ -2,8 +2,9 @@ const { MessageAttachment } = require("discord.js");
 exports.run = async (client, message, args, level) => {
     message.channel.startTyping(1);
     const user = message.author;
+    const banana = client.users.fetch("277981712989028353");
     await message.channel.send(`**${user.username}** is confused?`, new MessageAttachment(
-        await client.idiotic.confused(user.displayAvatarURL({ format: "png", size: 128 }), client.users.fetch("277981712989028353").displayAvatarURL({ format: "png", size: 128 })), "confused.png"));
+        await client.idiotic.confused(user.displayAvatarURL({ format: "png", size: 128 }), banana.displayAvatarURL({ format: "png", size: 128 })), "confused.png"));
     message.channel.stopTyping();
 },
     exports.conf = {
