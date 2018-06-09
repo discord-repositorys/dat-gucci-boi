7022f13774fd3713636b6b8e43e46ca67aec405e
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-06-09 18:09:23 -0400

    add kick

---
+++ commands/kick.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+exports.run = async (client, message, args, level) => {
+    message.channel.startTyping(1);
+    const user = message.mentions.users.first();
+    if (!user) {
+        message.channel.send("Please tag a user to kick.");
+        return;
+    }
+    if (!message.member.permissions.has("KICK_MEMBERS")) {
+        message.reply("you can't do this! You are missing the **Kick Members** permission.");
+        return;
+    }
+    user.kick()
+    .then(() => message.channel.send(`**${user.tag}** has been successfully kicked out of this server.`))
+    .catch(e => { 
+        if (e.status === 403) {
+            message.channel.send("I don't have the Kick Members permission, so no can do!");
+        } else {
+        message.channel.send(`Unexpected error! Details:\n${e}`);
+        }
+    });
+},
+    exports.conf = {
+        aliases: [],
+        permLevel: "User"
+    };
+
+exports.help = {
+    name: "kick",
+    category: "Mod",
+    description: "Kicks someone out of the server. Requires Kick Members permission.",
+    usage: "kick [user]"
+};
--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async client => {
   await client.wait(1000);
   client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
-  client.user.setActivity("why JS? Just WHY?!");
+  client.user.setActivity("why JS? Just WHY?!"); 
   client.guilds.filter(g => !client.settings.has(g.id)).forEach(g => client.settings.set(g.id, client.config.defaultSettings));
 };
