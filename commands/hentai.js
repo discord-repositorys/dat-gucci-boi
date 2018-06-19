6c121db6fc5262830b16ab56734d45e70b8e645d
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-06-19 15:41:31 -0400

    add hentai

---
+++ commands/hentai.js
@@ -0,0 +1,35 @@
+const {MessageEmbed} = require("discord.js");
+const superagent = require("superagent");
+
+
+exports.run = async (client, message, args, level) => {
+    message.channel.startTyping(1);
+    if(!message.channel.nsfw) {
+        message.channel.send("If I didn't stop you, everyone else seeing this **non-NSFW** channel would have eye cancer.");
+        return;
+    }
+    superagent.get("https://nekos.life/api/v2/img/Random_hentai_gif")
+    .then((resp) => {
+        const image = resp.body.url;
+        const embed = new MessageEmbed()
+            .setTitle("Here, have some gucci hentai.")
+            .setImage(image)
+            .setFooter(message.author.name, message.author.displayAvatarURL({ format: "png", size: 128 }));
+        message.channel.send({embed});
+    })
+    .catch((e) => {
+        message.channel.send(`An unexpected error occured. You shouldn't get this error:\n${e}`);
+    });
+    message.channel.stopTyping();
+},
+    exports.conf = {
+        aliases: [],
+        permLevel: "User"
+    };
+
+exports.help = {
+    name: "hentai",
+    category: "NSFW",
+    description: "Get a random hentai picture.",
+    usage: "hentai"
+};
