57b98adf7bc436877c00bc01f7141250eaae3dcf
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-06-19 21:27:20 -0400

    forgot to specify time

--- events/ready.js
+++ events/ready.js
@@ -11,6 +11,6 @@ module.exports = async client => {
   ];
   await client.wait(1000);
   client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
-  setInterval(() => client.user.setActivity(presences.random()));
+  setInterval(() => client.user.setActivity(presences.random()), 15000);
   client.guilds.filter(g => !client.settings.has(g.id)).forEach(g => client.settings.set(g.id, client.config.defaultSettings));
 };
